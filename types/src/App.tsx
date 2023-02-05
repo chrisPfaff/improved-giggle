407b5b5d2b3d5f747ba12b4a58f645a65aa152b5
chrisPfaff <chrispfaff10@gmail.com>
2023-02-05 17:24:00 -0600

    added button, changed to a controlled form avoided form validation so far

--- types/src/App.tsx
+++ types/src/App.tsx
@@ -2,61 +2,47 @@ import './App.css';
 
 import { useEffect, useState } from 'react';
 
+import Button from './components/Button';
 import Input from './components/Input';
 import Success from './components/Success';
 
 function App() {
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
-  useEffect(() => {
-    const getMessage = async () => {
-      try {
-        const response = await fetch('/api');
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.log('error');
-      }
-    };
-    getMessage();
-  }, []);
+  const handleClick = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+  };
+  // useEffect(() => {
+  //   const getMessage = async () => {
+  //     try {
+  //       const response = await fetch('/api/help');
+  //       const data = await response.json();
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.log('error');
+  //     }
+  //   };
+  //   getMessage();
+  // }, []);
 
-  console.log('render');
   return (
     <div className="App">
       <div className="inputs">
-        <Input text="Name :" forId="name">
-          <input
-            type="text"
-            id="name"
-            onChange={e => {
-              setUser({ ...user, name: e.currentTarget.value });
-            }}
-          />
-        </Input>
-        <Input text="Email :" forId="email">
-          <input
-            type="email"
-            id="email"
-            onChange={e => {
-              setUser({ ...user, email: e.currentTarget.value });
-            }}
-          />
-        </Input>
-        <Input text="Password :" forId="password">
-          <input
-            type="password"
-            id="password"
-            onChange={e => {
-              setUser({ ...user, password: e.currentTarget.value });
-            }}
-          />
-        </Input>
+        <form onSubmit={handleClick}>
+          <Input text="Name :" forId="name">
+            <input type="text" name="name" id="name" />
+          </Input>
+          <Input text="Email :" forId="email">
+            <input type="email" name="email" id="email" />
+          </Input>
+          <Input text="Password :" forId="password">
+            <input type="password" name="password" id="password" />
+          </Input>
+          <Button />
+        </form>
       </div>
-      {user.name && user.email && user.password ? <Success /> : null}
     </div>
   );
 }
