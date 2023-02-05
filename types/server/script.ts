0ab4b7ce99fc2a0c6845eeef6f390020a15abee3
chrisPfaff <chrispfaff10@gmail.com>
2023-02-05 17:24:26 -0600

    added bcrypt

--- types/server/script.ts
+++ types/server/script.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt';
 import express from 'express';
 import path from 'path';
+const saltRounds = 10;
 
 // const prisma = new PrismaClient()
 
@@ -25,7 +27,9 @@ const app = express();
 app.get('/api', () => {
   console.log("working")
 });
-
+app.get('/api/help', () => {
+  console.log("help")
+});
 if (process.env.NODE_ENV === 'production') {
   console.log(`__dirname = ${__dirname}`);
   app.use(express.static(path.join(__dirname, '..', 'dist')));
