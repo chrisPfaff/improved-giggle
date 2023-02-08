4bb6cfd1fc4041897fe0b15fc8d84404ab40a15d
chrisPfaff <chrispfaff10@gmail.com>
2023-02-08 14:08:23 -0600

    using local storage before I connect to postgres

--- types/src/App.tsx
+++ types/src/App.tsx
@@ -1,40 +1,11 @@
 import './App.css';
 
-import { useEffect, useState } from 'react';
-
-import Button from './components/Button';
-import Input from './components/Input';
-import Success from './components/Success';
-
-type signUpData = {
-  name: string;
-  email: string;
-  password: string;
-};
+import SignUp from './components/SignUp';
+import { checkAuth } from './utils/session';
+import { useState } from 'react';
 
 function App() {
-  const handleClick = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-    const apiCall = async ({ name, email, password }: signUpData) => {
-      try {
-        const response = await fetch('/api', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ name, email, password }),
-        });
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.log('error');
-      }
-    };
-    apiCall(data as signUpData);
-  };
+  const [auth, setAuth] = useState(checkAuth());
   // useEffect(() => {
   //   const getMessage = async () => {
   //     try {
@@ -48,24 +19,7 @@ function App() {
   //   getMessage();
   // }, []);
 
-  return (
-    <div className="App">
-      <div className="inputs">
-        <form onSubmit={handleClick}>
-          <Input text="Name :" forId="name">
-            <input type="text" name="name" id="name" />
-          </Input>
-          <Input text="Email :" forId="email">
-            <input type="email" name="email" id="email" />
-          </Input>
-          <Input text="Password :" forId="password">
-            <input type="password" name="password" id="password" />
-          </Input>
-          <Button />
-        </form>
-      </div>
-    </div>
-  );
+  return <div className="App">{auth ? <h1>Logged In</h1> : <SignUp />}</div>;
 }
 
 export default App;
--- types/src/components/Success.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-export default function Success({}: Props) {
-  return <div>Success</div>;
-}
