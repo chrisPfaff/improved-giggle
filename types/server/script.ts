572ed66319fe1f3404f5c41dfd7494a746138313
chrisPfaff <chrispfaff10@gmail.com>
2023-02-17 18:42:57 -0600

    server sends creds to railway

--- types/server/script.ts
+++ types/server/script.ts
@@ -4,21 +4,17 @@ import express from 'express';
 import path from 'path';
 const saltRounds = 10;
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-// async function main() {
-//   // ... you will write your Prisma Client queries here
-// }
-
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
+async function main(pass, name, email) {
+  await prisma.user.create({
+    data: {
+      email: email,
+      name: name,
+      password: pass,
+    },
+  });
+}
 
 const app = express();
 app.use(express.json());
@@ -28,6 +24,15 @@ app.post('/api', (req, res) => {
     if (err) {
       console.log(err);
     }
+    main(hash, req.body.email, req.body.name)
+      .then(async () => {
+        await prisma.$disconnect();
+      })
+      .catch(async e => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+      });
     res.json(hash);
   });
 });
